c697c58ec10e06caf617c2bf8c6f97bc2ccf3f46
Amo Chumber <amo.chumber@connect-group.com>
2016-06-10 00:48:58 +0100

    initial commit
    An attempt to create a useful default template for bash scripts.
    
    This template is not designed to be portable or POSIX compliant. It is
    for bash scripts and follows recommendations in the Google Shell Style
    Guide: google.github.io/styleguide/shell.xml
    
    Some of the decisions I made are below:
    
    * Using #!/usr/bin/env bash for "safety"
    
    * error if any of the follwoing happens: a command fails, a variable is
    * unset or a command in a pipechain fails
    
    * add a trap for CTRL-C
    
    * add some simple colors for logging - the less the better!
    
    * Have a "main" function to call other functions. This allows us to use
    * more local variables and thus increase "safety" of the script

---
+++ templates/bash_template.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+#
+#####
+# Describe your script here
+#
+#####
+
+# exit on commands that error, unset variables, commands in pipe chains that
+# fail
+set -euo pipefail
+
+# trap ctrl-c and call ctrl_c()
+ctrl_c()
+{
+  echo -e "\n\e[1;31mcaught CTRL-C - EXITING!\e[0m"
+  exit 1
+}
+
+trap ctrl_c SIGINT
+
+# set some colours for use in the script
+if tput setaf 1 &> /dev/null; then
+  tput sgr0;
+  bold=$(tput bold);
+  reset=$(tput sgr0);
+  red=$(tput setaf 160);
+  green=$(tput setaf 64);
+else
+  bold='';
+  reset="\e[0m";
+  red="\e[1;31m";
+  green="\e[1;32m";
+fi
+
+# add some outputting functions to print status, show errors, draw attention to
+# output and exit on errors
+# TODO: use the centos/debian logging functions libraries:-
+# /lib/lsb/init-functions or /etc/init.d/functions
+
+print_status()
+{
+  echo
+  echo -e "[INFO] $1"
+  echo
+}
+
+exit_on_error()
+{
+  echo -e "${red}${bold}[ERROR] Failure executing:${reset} $1"
+  exit 1
+}
+
+run_command_noexit()
+{
+  echo -ne "[INFO] Executing: ${1}..."
+  bash -c "$1"
+}
+
+run_command()
+{
+  $(run_command_noexit "$1"; echo -e "${green}[OK]${reset}") \
+  || exit_on_error "$1"
+}
+
+main()
+{
+true # replace "true" with your functions in here
+}
+
+main "$@"
